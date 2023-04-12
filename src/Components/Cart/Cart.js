c54fcb30984811ecae7d9677cf1cbbe2bd823e9c
BasementWoodworker <kefa1337@gmail.com>
2023-04-12 21:50:15 +0800

    Style shopping cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,15 +3,25 @@ import ItemInCart from "./ItemInCart";
 
 export default function Cart({ cart, setCart, setHideCart }) {
   const total = cart.reduce((summ, item) => summ += item.price * item.quantity, 0);
-  console.log("Rendering")
+  const emptyMsg = (
+    <div className="empty-message">
+      <span className="msg">Empty</span>
+      <span className="icon"></span>
+    </div>
+  )
+  const checkout = (
+    <div className="checkout-container">
+      <div className="total">Total: {total}$</div>
+      <button className="checkout" disabled={total === 0}>Checkout</button>
+    </div>
+  )
 
   return(
     <div className="cart-container modal" onClick={() => setHideCart(true)}>
       <div className="cart" onClick={(event) => event.stopPropagation()}>
         <div className="cart-close" onClick={() => setHideCart(true)}></div>
         {cart.map((item) => <ItemInCart itemInfo={item} cart={cart} setCart={setCart}/>)}
-        <div>Total: {total}$</div>
-        <button disabled={total === 0}>Checkout</button>
+        {cart.length !== 0 ? checkout : emptyMsg}
       </div>
     </div>
   )
--- src/Components/Cart/ItemInCart.js
+++ src/Components/Cart/ItemInCart.js
@@ -15,19 +15,39 @@ export default function ItemInCart({ itemInfo, cart, setCart }) {
 
   function increment() {
     const itemInCart = cart.find(elem => elem.id === id);
-    const itemIndex = cart.indexOf(itemInCart);
-    const itemCopy = itemInCart;
+    const itemCopy = {...itemInCart};
     itemCopy.quantity++;
+    const itemIndex = cart.indexOf(itemInCart);
     setCart(cart.slice(0, itemIndex).concat(itemCopy).concat(cart.slice(itemIndex + 1)));
   }
   function decrement() {
     const itemInCart = cart.find(elem => elem.id === id);
-    if (itemInCart.quantity === 1) return;
-    const itemIndex = cart.indexOf(itemInCart);
-    const itemCopy = itemInCart;
+    if (itemInCart.quantity <= 1) return;
+    const itemCopy = {...itemInCart};
     itemCopy.quantity--;
+    const itemIndex = cart.indexOf(itemInCart);
     setCart(cart.slice(0, itemIndex).concat(itemCopy).concat(cart.slice(itemIndex + 1)));
   }
+  function handleInputChange(e) {
+    const input = Number(e.target.value)
+    if (isNaN(input)) {
+      e.preventDefault();
+      return;
+    }
+    const itemInCart = cart.find(elem => elem.id === id);
+    const itemCopy = {...itemInCart};
+    itemCopy.quantity = input;
+    const itemIndex = cart.indexOf(itemInCart);
+    setCart(cart.slice(0, itemIndex).concat(itemCopy).concat(cart.slice(itemIndex + 1)));
+  }
+  function removeZeroQuantityItems(e) {
+    const input = Number(e.target.value)
+    if (input != 0) return;
+    const itemInCart = cart.find(elem => elem.id === id);
+    const itemIndex = cart.indexOf(itemInCart);
+    setCart(cart.slice(0, itemIndex).concat(cart.slice(itemIndex + 1)));
+  }
+
   function remove() {
     setCart(cart.filter(elem => elem.id !== id));
   }
@@ -35,15 +55,16 @@ export default function ItemInCart({ itemInfo, cart, setCart }) {
   return(
   <div className="item-container-cart">
     <img src={require(`../../Assets/images/products/${wood.toLowerCase()}-${shape}.jpg`)}></img>
-    <p>{`${wood} ${shape} ${thickness} x ${width} x ${length}`}</p>
-    <p>{moisture + "% moisture"}</p>
-      <span>{price + "$"}</span>
-      <div className="quantity-container">
+    <p className="name">{`${wood} ${shape} ${thickness} x ${width} x ${length}`}</p>
+    <div className="right-part">
+      <span className="price">{price * quantity + "$"}</span>
+      <div className="quantity-container" onClick={e => e.preventDefault()}>
         <button onClick={decrement}>−</button>
-        <div>Quantity: {quantity}</div>
+        <input min={1} value={quantity} onChange={handleInputChange} onBlur={removeZeroQuantityItems}/>
         <button onClick={increment}>+</button>
       </div>
-      <button onClick={remove}>Remove</button>
+      <button className="remove" onClick={remove}>Remove</button>
+    </div>
   </div>
   )
 }
