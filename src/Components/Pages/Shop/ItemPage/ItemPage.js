3c8327d64a10c7f3fcd382c5b129cf72594ce028
BasementWoodworker <kefa1337@gmail.com>
2023-04-12 19:48:48 +0800

    Style item page

--- src/Components/Pages/Shop/ItemPage/ItemPage.js
+++ src/Components/Pages/Shop/ItemPage/ItemPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import itemData from "../../../../ItemData/ItemData.json";
 import woodDescriptions from "../../../../ItemData/woodDescriptions.json";
@@ -17,6 +17,23 @@ export default function ItemPage({ addToCart }) {
   } = itemInfo
   const description = woodDescriptions[wood];
 
+  useEffect(() => window.scrollTo(0, 0), []);
+
+  const [currentQuantity, setCurrentQuantity] = useState(1);
+  const positiveWholeNumbers = new RegExp(/^0*[1-9]\d*$/);
+  function increment() {
+    if (!positiveWholeNumbers.test(currentQuantity)) setCurrentQuantity(1)
+    else setCurrentQuantity(currentQuantity + 1)
+  }
+  function decrement() {
+    if (!positiveWholeNumbers.test(currentQuantity)) setCurrentQuantity(1)
+    else setCurrentQuantity(currentQuantity === 1 ? 1 : currentQuantity - 1);
+  }
+  function handleInputChange(e) {
+    const input = Number(e.target.value)
+    if (!isNaN(input)) setCurrentQuantity(Number(e.target.value))
+  }
+
   return(
     <main className="item-page">
       <img className="image" src={require(`../../../../Assets/images/products/${wood.toLowerCase()}-${shape}.jpg`)}/>
@@ -24,25 +41,43 @@ export default function ItemPage({ addToCart }) {
         <h2 className="name">{`${wood} ${shape} ${thickness} x ${width} x ${length}`}</h2>
         <div className="price">{"$" + price}</div>
         <table>
-          <tr>
-            <td>Thickness</td>
-            <td>{thickness} mm</td>
-          </tr>
-          <tr>
-            <td>Width</td>
-            <td>{width} mm</td>
-          </tr>
-          <tr>
-            <td>Length</td>
-            <td>{length} mm</td>
-          </tr>
-          <tr>
-            <td>Moisture content</td>
-            <td>{moisture}%</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Thickness</td>
+              <td>{thickness} mm</td>
+            </tr>
+            <tr>
+              <td>Width</td>
+              <td>{width} mm</td>
+            </tr>
+            <tr>
+              <td>Length</td>
+              <td>{length} mm</td>
+            </tr>
+            <tr>
+              <td>Moisture content</td>
+              <td>{moisture}%</td>
+            </tr>
+          </tbody>
         </table>
-        <button onClick={() => addToCart(itemInfo)}>ADD TO CART</button>
-        <p>{description}</p>
+        <div className="button-container">
+        <div className="quantity-container" onClick={e => e.preventDefault()}>
+          <button onClick={decrement}>−</button>
+          <input min={1} value={currentQuantity} onChange={handleInputChange}/>
+          <button onClick={increment}>+</button>
+        </div>
+        <button
+          className="add-in-itempage"
+          onClick={
+            (e) => {
+              e.preventDefault(); // Don't open item's page
+              if (addToCart(itemInfo, currentQuantity) === "success") setCurrentQuantity(1);
+            }
+          }
+        >Add to cart
+        </button>
+      </div>
+        <p className="description">{description}</p>
       </div>
     </main>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath: "/"
   },
   plugins: [
     new HtmlWebpackPlugin({
